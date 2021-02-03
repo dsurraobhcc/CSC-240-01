e3da4f531e3f1422b59e4b7994075ff0700b126f
dominic.surrao@bhcc.edu <dsurrao@dominics-mbp.myfiosgateway.com>
2021-02-03 17:49:02 -0500

    class3

--- classes/class1/class1_attendance.sql
+++ classes/class1/class1_attendance.sql
@@ -10,11 +10,29 @@ create table attendance (
     class_description varchar(50)
 );
 
+create table student (
+	student_id int unsigned primary key auto_increment,
+    last_name varchar(50) not null,
+    first_name varchar(50) not null
+);
+
+create table attendance_student (
+	student_id int unsigned not null,
+    attendance_id mediumint unsigned not null,
+    attendance_status enum ('present', 'absent') not null,
+    foreign key (student_id) references student(student_id),
+    foreign key (attendance_id) references attendance(attendance_id) 
+		on delete cascade,
+	unique key (student_id, attendance_id)
+);
+
 create table academic_calendar (
 	calendar_id mediumint unsigned primary key auto_increment,
     event_date date,
     event_description varchar(1000)
-)
+);
+
+
 
 
 
---
+++ classes/class3/scripts/class1_attendance_data.sql
@@ -0,0 +1,48 @@
+use scheduling;
+
+delete from attendance;
+
+insert into attendance (class_date, class_time, class_type, class_description)
+values ('2021-01-27', '4:30 - 5:45pm', 'All students', 'Regular session'),
+('2021-02-01', '4:30 - 5:45pm', 'All students', 'Regular session'),
+('2021-02-03', '4:30 - 5:45pm', 'All students', 'Regular session'),
+('2021-02-15', '4:30 - 5:45pm', 'All students', 'Regular session');
+
+insert into attendance (class_date, class_time, class_type, class_description)
+values ('2021-02-03', '4:30 - 5:45pm', 'All students', 'Regular session');
+
+set @attendance_id = last_insert_id();
+
+insert into student (last_name, first_name)
+values ('Vaughn', 'Vince'), ('Kim', 'Jane');
+
+-- Vince id 1, Jane id 2
+-- class on 2/3 attendance id 11
+
+-- Mark Vince as present
+insert into attendance_student (student_id, attendance_id, attendance_status)
+values (1, @attendance_id, 'present');
+
+-- Mark Jane as absent
+insert into attendance_student (student_id, attendance_id, attendance_status)
+values (2, @attendance_id, 'absent');
+
+insert into attendance_student (student_id, attendance_id, attendance_status)
+values (2, @attendance_id, 'present');
+
+Error Code: 1062. Duplicate entry '2-5' for key 'student_id'
+
+
+-- delete a session
+delete from attendance 
+where attendance_id = 5;
+
+select * from attendance;
+select * from attendance_student;
+
+insert into student (last_name, first_name)
+values (NULL, NULL);
+
+select * from student;
+
+
